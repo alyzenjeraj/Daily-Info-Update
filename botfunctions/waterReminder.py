aafb1b6f09e44d2a08fdcd871f9ce08df81e4e62
Kevin Wang <70186166+ViridianCitrus@users.noreply.github.com>
2021-06-26 15:36:58 -0600

    init daily reminder

--- botfunctions/waterReminder.py
+++ botfunctions/waterReminder.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 def checkTime():
-    if datetime.now().strftime("%H") >= 7 and datetime.now().strftime("%H") <= 11 and datetime.now().strftime("%M") == 30:
+    if int(datetime.now().strftime("%H")) >= 7 and int(datetime.now().strftime("%H")) <= 11 and int(datetime.now().strftime("%M")) == 9:
         print("Drink Water!")
     else:
         pass
